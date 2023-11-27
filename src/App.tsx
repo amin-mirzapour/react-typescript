b0c1fc0deaf5561129fd2970884535fa80a487de
Amin Mirzapour <aminmrzpr@gmail.com>
2023-11-27 18:44:33 +0330

    advanced props

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,42 @@
 import './App.css';
 import Greet from './components/Greet';
+import Heading from './components/Heading';
+import Oscar from './components/Oscar';
 import Person from './components/Person';
 import PersonList from './components/PersonList';
+import Status from './components/Status';
 
 function App() {
-  const personName = {
-    first: 'Amin',
-    last: 'Mir',
-  };
-  const nameList = [
-    {
-      first: 'Hidi',
-      last: 'Gh',
-    },
-    {
-      first: 'Soli',
-      last: 'Ma',
-    },
-    {
-      first: 'Mosi',
-      last: 'Go',
-    },
-  ];
+  // const personName = {
+  //   first: 'Amin',
+  //   last: 'Mir',
+  // };
+  // const nameList = [
+  //   {
+  //     first: 'Hidi',
+  //     last: 'Gh',
+  //   },
+  //   {
+  //     first: 'Soli',
+  //     last: 'Ma',
+  //   },
+  //   {
+  //     first: 'Mosi',
+  //     last: 'Go',
+  //   },
+  // ];
 
   return (
     <div className="App">
-      <Greet name="Amin" messageCount={20} isLoggedIn={true} />
+      {/* <Greet name="Amin" messageCount={20} isLoggedIn={true} />
       <Person name={personName} />
-      <PersonList names={nameList} />
+      <PersonList names={nameList} /> */}
+      <Status status='loading' />
+      <Heading>PlaceHolder Text</Heading>
+      <Oscar>
+        <Heading>Oscar Goes to Dicaprio!</Heading>
+      </Oscar>
+      <Greet name='Amin' />
     </div>
   );
 }
--- src/components/Greet.tsx
+++ src/components/Greet.tsx
@@ -1,13 +1,14 @@
 type GreetProps = {
     name : string,
-    messageCount : number
-    isLoggedIn : boolean
+    messageCount? : number
+    isLoggedIn?: boolean
 }
 
 const Greet = (props : GreetProps) => {
+  const {messageCount = 0} = props
   return (
     <div>
-        <h2>{props.isLoggedIn ? `Welcome ${props.name}, you have ${props.messageCount} new messages` : `welcome geust`}</h2>
+        <h2>{props.isLoggedIn ? `Welcome ${props.name}, you have ${messageCount} new messages` : `welcome geust`}</h2>
     </div>
   )
 }
